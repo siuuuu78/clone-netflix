653d87d2d84cddba9cb8fd3c325113a44bd9ce7a
siuuuu78 <riyanharun4@gmail.com>
2024-11-30 22:37:44 +0700

    update on Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -12,9 +14,13 @@ const Home = () => {
         <img src={hero_banner} alt="" className='banner-img' />
         <div className="hero-caption">
           <img src={hero_title} alt="" className='caption-img' />
-          <p>Discovering his ties to a secret ancient order, yanto, an unassuming archaeologist, 
+          <p>Discovering his ties to a secret ancient order, Jayusman, an archaeologist, 
             stumbles upon an artifact that sparks strange visions of a world long lost. 
              </p>
+             <div className="hero-btn">
+              <button className='btn'><img src={play_icon} alt="" />Play</button>
+              <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+             </div>
         </div>
       </div>
     </div>
