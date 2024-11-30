32ffeee22b591bb5be064475906d1a5e5492ec01
siuuuu78 <riyanharun4@gmail.com>
2024-12-01 01:09:59 +0700

    update card on Home

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list">
         {cards_data.map((card, index) => {
           return <div className="card" key={index}>
