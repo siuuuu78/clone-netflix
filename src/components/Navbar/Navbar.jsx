e58d6ee43603517bb2813e8de45b35fa22e743ac
siuuuu78 <riyanharun4@gmail.com>
2024-11-30 21:54:40 +0700

    add banner on home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,6 +28,9 @@ const Navbar = () => {
         <div className='navbar-profile'>
         <img src={profile_img} alt="" className='profile' />
         <img src={caret_icon} alt=""  />
+        <div className="dropdown">
+          <p>Sign Out</p>
+        </div>
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <div className='hero'>
+        <img src={hero_banner} alt="" className='banner-img' />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img' />
+          <p>Discovering his ties to a secret ancient order, yanto, an unassuming archaeologist, 
+            stumbles upon an artifact that sparks strange visions of a world long lost. 
+             </p>
+        </div>
+      </div>
     </div>
   )
 }
