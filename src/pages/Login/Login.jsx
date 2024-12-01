e530661eb1b542bd974d0b20768089cd5c79da6d
siuuuu78 <riyanharun4@gmail.com>
2024-12-01 23:13:42 +0700

    styling login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,17 +7,18 @@ const Login = () => {
     <div className='login'>
     <img src={logo} className='login-logo' alt="" />
     <div className="login-form">
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
       <form >
         <input type="text" placeholder='Your name' />
         <input type="email" placeholder='Email' />
         <input type="password" placeholder='password' />
-        <button>Sign In</button>
+        <button>Sign Up</button>
         <div className="form-help">
           <div className="remember">
             <input type="checkbox" />
             <label htmlFor="">Remember Me</label>
           </div>
+          <p>Need Help?</p>
         </div>
       </form>
     </div>
