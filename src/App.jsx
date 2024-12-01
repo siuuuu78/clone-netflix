19a31763a5e3880d84defeb2edb1f08837943350
siuuuu78 <riyanharun4@gmail.com>
2024-12-01 22:26:00 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
