80e2679d1db32a8537ed9149541f6bf53013e989
siuuuu78 <riyanharun4@gmail.com>
2024-12-03 20:57:10 +0700

    add api movie database

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
+
+  const cardsRef = useRef(); 
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjdiY2QxMDEzZGEwYWMyZDMyNjZiYzBlNzJmMWNkNSIsIm5iZiI6MTczMzE4ODU2NC4zNDMwMDAyLCJzdWIiOiI2NzRlNWJkNDc5OWJjMDQ3MmRlZTljZmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.cKepMIfjKNTHdD_qlu68RQeSeHWPSAQ4uWTqq9sHPYQ'
+    }
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+  
+ useEffect(() => {
+
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
+  cardsRef.current.addEventListener('wheel', handleWheel);
+
+ },[])
+
   return (
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
-      <div className="card-list">
-        {cards_data.map((card, index) => {
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
